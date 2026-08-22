88a9fa4883429190aacc0c036c440d5324ed1ea4
wleka <116182950+wleka@users.noreply.github.com>
2026-08-22 20:08:57 +0500

    Updated ping a players, also updated config for pings * README.md updated

--- src/main/java/org/example/wleku/wlekus_chat/OtherEvents/playerPingEvent.kt
+++ src/main/java/org/example/wleku/wlekus_chat/OtherEvents/playerPingEvent.kt
@@ -1,11 +1,13 @@
 package org.example.wleku.wlekus_chat.OtherEvents
 
+import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.player.AsyncPlayerChatEvent
 import org.example.wleku.wlekus_chat.Wlekus_chat
+import org.bukkit.Sound
 
 class playerPingEvent(private var plugin: Wlekus_chat) : Listener {
 
@@ -14,10 +16,17 @@ class playerPingEvent(private var plugin: Wlekus_chat) : Listener {
         val message = event.message
 
         if (plugin.config.getBoolean("pingEvent") == true) {
-            for (player in Bukkit.getOnlinePlayers()) {
-                if (message.contains(player.name)) {
+            for (target in Bukkit.getOnlinePlayers()) {
+                if (message.contains(target.name)) {
                     event.message =
-                        message.replace("${player.name}", "${plugin.config.getString("pingColor")}${player.name}§r")
+                        message.replace("${target.name}", "${plugin.config.getString("pingColor")}${target.name}§r")
+                    target.playSound(target.location, Sound.ENTITY_PLAYER_LEVELUP, 1.0F, 1.0F)
+
+                    val replace = plugin.config.getString("barMessage")
+                        ?.replace("%target%", "${target.name}")
+                        ?.replace("%sender%", "${event.player.name}")
+
+                    target.sendActionBar(Component.text("$replace"))
                 }
             }
         }
